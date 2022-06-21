044493812311f1e3e8c681304304f6c554a3d87d
Adriano <adrianocardoso1991@gmail.com>
2022-06-20 23:08:18 -0300

    Created layout component tests

--- src/components/Layout/Layout.test.js
+++ src/components/Layout/Layout.test.js
@@ -1,12 +1,30 @@
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import Layout from "./Layout";
+import AuthPage from "../../pages/AuthPage";
 
 describe("Page content", () => {
-  test("renders home page link to unauthenticated user", () => {
+  test("renders login page passed as props.children", () => {
+    render(
+      <BrowserRouter>
+        <Layout>
+          <AuthPage />
+        </Layout>
+      </BrowserRouter>
+    );
+
+    const signUpText = screen.getByText("Create new account");
+    expect(signUpText).toBeInTheDocument();
+  });
+
+  test("renders navigation bar with home link", () => {
     render(
       <BrowserRouter>
         <Layout />
       </BrowserRouter>
     );
+
+    const homeLink = screen.getByText("Home");
+    expect(homeLink).toBeInTheDocument();
+  });
 });
