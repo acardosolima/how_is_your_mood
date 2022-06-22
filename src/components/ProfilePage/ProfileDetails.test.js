a325ac4376c5ef43af1f8baab659998b1ec64641
Adriano <adrianocardoso1991@gmail.com>
2022-06-22 14:02:55 -0300

    Added tests to AuthForm

--- src/components/ProfilePage/ProfileDetails.test.js
+++ src/components/ProfilePage/ProfileDetails.test.js
@@ -1,10 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import ProfileDetails from "./ProfileDetails";
 
+beforeEach(() => {
+  render(<ProfileDetails />);
+});
+
 describe("Cadastral information", () => {
   test("renders email registered by the user", () => {
-    render(<ProfileDetails />);
-
     const emailValue = screen.getByAltText("email");
     const emailRegex =
       /^([A-Za-z0-9_\-.+])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,})$/;
@@ -13,8 +15,6 @@ describe("Cadastral information", () => {
   });
 
   test("renders date of last visit", () => {
-    render(<ProfileDetails />);
-
     const lastVisitValue = screen.getByAltText("lastVisit");
     const lastVisitRegex = new RegExp("^\\d{4}.\\d{2}.\\d{2}$");
 
